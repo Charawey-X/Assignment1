7ff91200453d89ecbcc94af7009e3b69a22f70f1
Charawey-X <charawey@gmail.com>
2022-06-20 20:07:13 +0000

    add function and connon and connect to html

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+
+
+function greeting () {
+  const inputName = document.getElementById('name').value;
+  const greet = document.getElementById('greeting')
+  greet.textContent = "Hello "+inputName+ ", welcome to the bootcamp";
+}
